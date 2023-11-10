b5ed34d4f6842db233040b9e186db91d3eeafc09
Ye-2077 <12111813@mail.sustech.edu.cn>
2023-11-10 10:57:02 +0800

    renamed:    ros_arm/arm/urdf/arm_1.urdf -> ros_arm/arm/urdf/arm.urdf 	renamed:    ros_arm/arm/urdf/arm_1_gazebo.urdf -> ros_arm/arm/urdf/arm_gazebo.urdf 	renamed:    ros_arm/arm/xacro/arm_1.xacro -> ros_arm/arm/xacro/arm.xacro 	renamed:    ros_arm/config/controllers/arm_1_gazebo_control.yaml -> ros_arm/config/controllers/arm_gazebo_control.yaml 	renamed:    ros_arm/config/rviz/arm_1.rviz -> ros_arm/config/rviz/arm.rviz 	renamed:    ros_arm/launch/arm_1_gazebo.launch -> ros_arm/launch/arm_gazebo.launch 	renamed:    ros_arm/launch/arm_1_gazebo_control.launch -> ros_arm/launch/arm_gazebo_control.launch 	renamed:    ros_arm/launch/arm_1_rviz.launch -> ros_arm/launch/arm_rviz.launch 	renamed:    ros_arm/src/mover.py -> ros_arm/src/test_mover.py
    	modified:   Moveit/moveit_resources (untracked content)
    	modified:   Moveit/panda_moveit_config (modified content)

--- ros_arm/src/test_mover.py
+++ ros_arm/src/test_mover.py
@@ -4,15 +4,12 @@ import math
 import rospy
 from std_msgs.msg import Float64
 
+
 def mover():
-    pub_j1 = rospy.Publisher('/arm/joint1_position_controller/command',
-                             Float64, queue_size=10)
-    pub_j2 = rospy.Publisher('/arm/joint2_position_controller/command',
-                             Float64, queue_size=10)
-    pub_j3 = rospy.Publisher('/arm/joint3_position_controller/command',
-                             Float64, queue_size=10)
-    pub_j4 = rospy.Publisher('/arm/joint4_position_controller/command',
-                             Float64, queue_size=10)
+    pub_j1 = rospy.Publisher('/arm/joint1_position_controller/command', Float64, queue_size=10)
+    pub_j2 = rospy.Publisher('/arm/joint2_position_controller/command', Float64, queue_size=10)
+    pub_j3 = rospy.Publisher('/arm/joint3_position_controller/command', Float64, queue_size=10)
+    pub_j4 = rospy.Publisher('/arm/joint4_position_controller/command', Float64, queue_size=10)
     rospy.init_node('arm_mover')
     rate = rospy.Rate(10)
     start_time = 0
@@ -28,8 +25,9 @@ def mover():
         pub_j4.publish(math.sin(2*math.pi*0.1*elapsed)*(math.pi/2))
         rate.sleep()
 
+
 if __name__ == '__main__':
     try:
         mover()
     except rospy.ROSInterruptException:
-         pass
+        pass
